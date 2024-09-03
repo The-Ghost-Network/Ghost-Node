3fd58ba3c4123d6a8ede1c429b8fa46710ee409f
Nobodycares-lo <140128868+Nobodycares-lo@users.noreply.github.com>
2024-09-03 00:09:04 -0700

    Create uv.config.js

---
+++ public/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+	prefix: "/~/uv/",
+	encodeUrl: Ultraviolet.codec.base64.encode,
+	decodeUrl: Ultraviolet.codec.base64.decode,
+	handler: "/uv/uv.handler.js",
+	client: "/uv/uv.client.js",
+	bundle: "/uv/uv.bundle.js",
+	config: "/uv/uv.config.js",
+	sw: "/uv/uv.sw.js",
+};
